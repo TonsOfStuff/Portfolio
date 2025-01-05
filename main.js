99f82c1f5d491509fafe863f3ea304b3a4ab682d
TonsOfStuff <jtons2008@gmail.com>
2025-01-05 16:35:43 -0600

    stars

--- main.js
+++ main.js
@@ -2,26 +2,55 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-mainBody.appendChild( renderer.domElement );
+document.getElementById("threeCanvas").appendChild( renderer.domElement );
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true;
 controls.dampingFactor = 0.12;
 
+//Window resizing
+window.addEventListener( 'resize', onWindowResize, false );
 
-const box = new THREE.Mesh(
-    new THREE.BoxGeometry(2, 2, 2),
-    new THREE.MeshBasicMaterial( {color: 0x00ff00} )
-)
+function onWindowResize(){
 
-scene.add(box);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
 
-camera.position.z += 5;
+
+
+
+
+
+
+
+
+for (let i = 0; i <= 10; i++){
+    const star = new THREE.Mesh(
+        new THREE.IcosahedronGeometry(2, 1),
+        new THREE.MeshBasicMaterial( {color: 0x00ff00} )
+    )
+
+    star.position.x = Math.random() * 100 - Math.random() * 100;
+    star.position.y = Math.random() * 100 - Math.random() * 100;
+    star.position.z = Math.random() * 100 - Math.random() * 100;
+
+
+    scene.add(star);
+}
+
+
+
+
+
+camera.position.z += 100;
 
 
 
